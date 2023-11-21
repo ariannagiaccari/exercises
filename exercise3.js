56869658a3c59c4370d7f71e992c84cc325cfb83
ariannagiaccari <150803603+ariannagiaccari@users.noreply.github.com>
2023-11-21 19:52:17 +0100

    Add files via upload

---
+++ exercise3.js
@@ -0,0 +1,5 @@
+//Utilizza il metodo forEach che prende un array di nomi di frutta
+//e li stampa aggiungendo un punto esclamativo a ciascun nome di frutta.
+
+const fruits = ["Mela", "Banana", "Kiwi", "Fragola"];
+fruits.forEach(fruit => console.log(fruit + "!"));
