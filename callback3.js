6e385bc1a24512c8e9adfa14352cdc53e667c526
ariannagiaccari <150803603+ariannagiaccari@users.noreply.github.com>
2023-12-02 16:11:52 +0100

    Add files via upload

--- callback3.js
+++ callback3.js
@@ -1,9 +1,9 @@
-function filterArray(arr, callback) { 
-    return callback(arr);
+function filterArray(arr, callback) {
+    const evenNums = arr.filter((num) => num % 2 === 0);
+    return evenNums;
 }
-
 function isEven(num) {
-    return numbers.filter(num => num % 2 === 0);
+    return num;
 }
 
 const numbers = [1, 2, 3, 4, 5, 6];
